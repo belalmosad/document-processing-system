a10753aab89406e38de1b5245651ae307b6f2d0b
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-28 07:56:11 +0300

    doc-processing: Fix import in user model and make cert filed to be nullable

--- document-processing-backend/app/db/models/user.py
+++ document-processing-backend/app/db/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from app.db.base import Base
+from db.base import Base
 from sqlalchemy.orm import relationship
 
 class User(Base):
@@ -9,7 +9,7 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     role = Column(String, nullable=False)
     passwordHash = Column(String, nullable=False)
-    certSubjectDN = Column(String, nullable=False)
+    certSubjectDN = Column(String, nullable=True)
 
     documents = relationship("DocumentMetadata", back_populates="author")
     document_permissions = relationship("DocumentUserPermission", back_populates="user")
