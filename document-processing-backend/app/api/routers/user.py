86f9beef5f825e1eaa5c881c6bd738b4fc8101bf
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-28 23:21:34 +0300

    doc-processing: Update route name to be /signup and route handler function name

--- document-processing-backend/app/api/routers/user.py
+++ document-processing-backend/app/api/routers/user.py
@@ -1,16 +1,23 @@
 from fastapi import APIRouter, Depends
 from api.services.user import UserService
-from api.schemas.user import UserCreate
+from api.schemas.user import UserCreate, UserLogin, UserResponse
 from sqlalchemy.orm import Session
 from db.session import get_db
+from core.dependencies import get_user_service
 
 router = APIRouter()
 
-@router.post("/")
-def create_user_router(user: UserCreate, db: Session = Depends(get_db)):
+@router.post("/signup")
+def user_signup_route(user: UserCreate, db: Session = Depends(get_db)):
     user_service = UserService(db)
     db_user = user_service.create_user(user)
     return {
         "Success": True,
         "Message": f"User {db_user.username} successfully created"
     }
+
+@router.post("/login")
+def user_login_route(user: UserLogin, user_service: UserService = Depends(get_user_service)):
+    access_token = user_service.login_user(user)
+    return {"access_token": access_token}
+    
