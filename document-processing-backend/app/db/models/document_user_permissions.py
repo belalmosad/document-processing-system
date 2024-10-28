a4e5e48dc900c601997d5dc02d302ec930e3035f
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-28 07:55:32 +0300

    doc-processing: Fix import in document_user_permission

--- document-processing-backend/app/db/models/document_user_permissions.py
+++ document-processing-backend/app/db/models/document_user_permissions.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from app.db.base import Base
+from db.base import Base
 
 class DocumentUserPermission(Base):
     __tablename__ = "document_user_permissions"
