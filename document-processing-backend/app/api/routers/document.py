828c4f6362ea564b5252e7570f4ff9e807a8f065
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-29 01:49:19 +0300

    doc-processing: Implement /upload endpoint and basic route handler

--- document-processing-backend/app/api/routers/document.py
+++ document-processing-backend/app/api/routers/document.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from core.dependencies import get_document_service
+from api.services.document import DocumentService
 
 router = APIRouter()
-@router.get("/")
-def getUser():
-    return {"Hello": "doc"}
+
+@router.post("/upload")
+def upload_and_process_document_router(document_data, document_service: DocumentService = Depends(get_document_service)):
+    return document_service.process_document("pdf", document_data)
