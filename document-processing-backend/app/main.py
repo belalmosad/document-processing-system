b7224daf8413a58142fa10885057fe3ee98d9b6b
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-28 07:57:42 +0300

    doc-processing: Use application middleware and removing unnecessary endpoint

--- document-processing-backend/app/main.py
+++ document-processing-backend/app/main.py
@@ -2,17 +2,19 @@ import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import uvicorn
-from app.api.routers import user, document
+from api.routers import user, document
+from db.base import import_models
+from middleware.exception_handling import ExceptionHandlingMiddleware
 
 
 app = FastAPI()
+app.add_middleware(ExceptionHandlingMiddleware)
+import_models() 
+
 app.include_router(user.router, prefix="/api/v1/users", tags=["Users"])
 app.include_router(document.router, prefix="/api/v1/documents", tags=["Documents"])
-load_dotenv()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "From another backend"}
+load_dotenv()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", 3001)), reload=os.getenv("DEBUG"))
