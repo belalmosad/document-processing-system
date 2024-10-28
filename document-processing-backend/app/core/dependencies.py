b6448987e46adec0d6371db1d04439341b88e54a
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-29 01:51:22 +0300

    doc-processing: Updata dependencies.py to include DocumentFactory and DocumentProcessor methods

--- document-processing-backend/app/core/dependencies.py
+++ document-processing-backend/app/core/dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 from api.services.user import UserService
 from api.services.authentication import AuthenticationService
+from api.services.document import DocumentFactory, DocumentService
 from sqlalchemy.orm import Session
 from db.session import get_db
 
@@ -11,3 +12,11 @@ def get_user_service(
     db: Session = Depends(get_db),
     auth_service: AuthenticationService = Depends(get_auth_service)):
     return UserService(db, auth_service)
+
+def get_document_factory():
+    return DocumentFactory()
+
+def get_document_service(
+    db: Session = Depends(get_db),
+    document_factory: DocumentFactory = Depends(get_document_factory)):
+    return DocumentService(db, document_factory)
