dd4662f995d970af3cfb2d3d3bc1a6ed98b3dadb
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-28 08:11:14 +0300

    doc-processing: Refactor user service to be a class UserService that has db instance contains service methods

--- document-processing-backend/app/api/services/user.py
+++ document-processing-backend/app/api/services/user.py
@@ -2,17 +2,20 @@ from api.schemas.user import UserCreate
 from sqlalchemy.orm import Session
 from db.models.user import User
 
-def create_user(db: Session, user_data: UserCreate) -> User:
-    try:
-        db_user = User(
-        username = user_data.username,
-        role = user_data.role,
-        passwordHash = user_data.password
-        )
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-        return db_user
-    except Exception as e:
-        raise Exception("Something went wrong")
-    
+class UserService:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_user(self, user_data: UserCreate) -> User:
+        try:
+            db_user = User(
+            username = user_data.username,
+            role = user_data.role,
+            passwordHash = user_data.password
+            )
+            self.db.add(db_user)
+            self.db.commit()
+            self.db.refresh(db_user)
+            return db_user
+        except Exception as e:
+            raise Exception("Something went wrong")
