36c1baf35993c122968fd8c88392b4e37a6d6acf
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-30 02:32:39 +0300

    doc-processing: Implement user authorization functionality to authorize document viewing

--- document-processing-backend/app/api/routers/document.py
+++ document-processing-backend/app/api/routers/document.py
@@ -3,7 +3,7 @@ from fastapi.responses import StreamingResponse
 from core.dependencies import get_document_service
 from api.services.document import DocumentService
 from api.schemas.document import DocumentMetadataResponse
-from core.security import auth_guard
+from core.security import auth_guard, authorize_to_show_document
 
 router = APIRouter(dependencies=[Depends(auth_guard)])
 
@@ -14,14 +14,16 @@ async def upload_and_process_document_router(
     document_metadata = await document_service.process_document(file)
     return document_metadata
 
-@router.get("/metadata/{document_id}")
-def get_document_metadata_by_id_router(document_id: int, document_service: DocumentService = Depends(get_document_service)):
+@router.get("/metadata/{document_id}", dependencies=[Depends(authorize_to_show_document)])
+def get_document_metadata_by_id_router(
+    document_id: int, 
+    document_service: DocumentService = Depends(get_document_service)):
     return document_service.get_document_metadata_by_document_id(document_id)
 
 @router.get("/metadata/author/all")
 def get_document_metadata_by_author_id_router(document_service: DocumentService = Depends(get_document_service)):
     return document_service.get_document_by_author_id(28)
 
-@router.get("/{document_id}")
+@router.get("/{document_id}", dependencies=[Depends(authorize_to_show_document)])
 def get_document_url_router(document_id: int, document_service: DocumentService = Depends(get_document_service)) -> StreamingResponse:
     return document_service.stream_document(document_id)
--- document-processing-backend/app/core/security.py
+++ document-processing-backend/app/core/security.py
@@ -1,6 +1,12 @@
-from fastapi import Depends, HTTPException, Request, status
+from fastapi import Depends, HTTPException, Request, Response, status
+from sqlalchemy import exists, or_
 from api.services.authentication import AuthenticationService
 from core.dependencies import get_auth_service
+from sqlalchemy.orm import Session
+from db.models.document_metadata import DocumentMetadata
+from db.models.document_user_permissions import DocumentUserPermission
+from db.session import get_db
+
 
 async def get_token_from_header(request: Request):
     auth_header = request.headers.get("Authorization")
@@ -13,7 +19,10 @@ async def get_token_from_header(request: Request):
     return token
 
 
-async def auth_guard(token: str = Depends(get_token_from_header), auth_service: AuthenticationService = Depends(get_auth_service)):
+async def auth_guard(
+    request: Request,
+    token: str = Depends(get_token_from_header), 
+    auth_service: AuthenticationService = Depends(get_auth_service)):
     payload = auth_service.verify_token(token)
     print(payload)
     if payload is None:
@@ -21,4 +30,29 @@ async def auth_guard(token: str = Depends(get_token_from_header), auth_service: 
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid authentication credentials"
         )
+    request.state.user = payload
+    print("payload")
+    print(payload)
     return payload
+
+async def authorize_to_show_document(request: Request, document_id: int, session: Session = Depends(get_db)):
+    user_id = request.state.user["user_id"]
+    print(user_id)
+    print(document_id)
+    is_authorized = session.query(
+        exists().where(
+            or_(
+                DocumentMetadata.author_id == user_id,
+                session.query(DocumentUserPermission).filter_by(
+                    document_id=document_id, user_id=user_id
+                ).exists()
+            )
+        )
+    ).scalar()
+    
+    if not is_authorized:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access required to this document"
+        )
+    return is_authorized
