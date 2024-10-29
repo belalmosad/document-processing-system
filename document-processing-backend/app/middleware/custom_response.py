6ba5c25ac94077fb64cce493efc9db91c63b7662
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-29 20:54:56 +0300

    doc-processing: Refactor custom response middleware to handle stream responses

--- document-processing-backend/app/middleware/custom_response.py
+++ document-processing-backend/app/middleware/custom_response.py
@@ -1,13 +1,17 @@
 import json
 import time
+from typing import AsyncGenerator
 from fastapi import Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, StreamingResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 
 class CustomResponseMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         start_time = time.time()
         response = await call_next(request)
+        if isinstance(response.body_iterator, AsyncGenerator):
+            response.headers["X-Process-Time"] = f"{(time.time() - start_time) * 1000:.2f} ms"
+            return response
         body = b""
         async for chunk in response.body_iterator:
             body += chunk
