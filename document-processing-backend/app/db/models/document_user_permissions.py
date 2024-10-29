4b8f81962c3261384da1db0d690c0f1ed182a0cb
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-29 23:49:18 +0300

    doc-processing: Update model DocumentUserPermission by removing id and use composite key of two foreig keys user_id and document_id

--- document-processing-backend/app/db/models/document_user_permissions.py
+++ document-processing-backend/app/db/models/document_user_permissions.py
@@ -4,10 +4,9 @@ from db.base import Base
 
 class DocumentUserPermission(Base):
     __tablename__ = "document_user_permissions"
-
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    document_id = Column(Integer, ForeignKey("documents.id"), nullable=False)
+    
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True, nullable=False)
+    document_id = Column(Integer, ForeignKey("documents.id"), primary_key=True, nullable=False)
     permission = Column(String, nullable=False)
 
     user = relationship("User", back_populates="document_permissions")
