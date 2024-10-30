298e14a22d4d1b87740203eacb68355792b61628
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-10-30 20:03:24 +0300

    Add authorization bearer header to openapi docs for swagger

--- document-processing-backend/app/main.py
+++ document-processing-backend/app/main.py
@@ -21,10 +21,24 @@ app.add_middleware(AuditTrailMiddleware)
 
 import_models() 
 
-app.include_router(user.router, prefix="/api/v1/users", tags=["Users"])
-app.include_router(document.router, prefix="/api/v1/documents", tags=["Documents"])
-app.include_router(admin.router, prefix="/api/v1/admin", tags=["Admin"])
-app.include_router(external_api.router, prefix="/api/v1/extra")
+app.include_router(admin.router, prefix="/admin", tags=["Admin"])
+app.include_router(user.router, prefix="/users", tags=["Users"])
+app.include_router(document.router, prefix="/documents", tags=["Documents"])
+app.include_router(external_api.router, prefix="/extra", tags=["External"])
+
+openapi_schema = app.openapi()
+openapi_schema["components"]["securitySchemes"] = {
+    "BearerAuth": {
+        "type": "http",
+        "scheme": "bearer",
+        "bearerFormat": "JWT"
+    }
+}
+for path in openapi_schema["paths"].values():
+    for method in path.values():
+        method["security"] = [{"BearerAuth": []}]
+
+app.openapi_schema = openapi_schema
 
 load_dotenv()
 
